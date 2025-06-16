55f973367154f31039ab6dea9dd71e36e2c6e842
Satyam Rai <satyamrai553@gmail.com>
2025-06-16 19:55:34 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+  
+
+  return (
+    <>
+   <div className="bg-amber-200 h-screen flex items-center justify-center">
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </div>
+    </>
+  )
+}
+
+export default App
